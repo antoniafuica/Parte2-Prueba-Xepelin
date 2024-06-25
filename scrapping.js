7df245e7a2f2d832b9c3427b4824145a7bab67a8
antoniafuica <afuica@uc.cl>
2024-06-25 16:21:59 -0400

    arreglos en puppeter.config

--- scrapping.js
+++ scrapping.js
@@ -13,11 +13,7 @@ const sheets = google.sheets({version: 'v4', auth: client});
 
 const scrappingBlog = async (category) => {
     console.log(`Empezando el browser con categoria: ${category}`);
-    const browser = await puppeteer.launch({
-        headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-        executablePath: '/opt/render/.cache/puppeteer/chrome-linux/chrome' 
-    });
+    const browser = await puppeteer.launch({headless: true});
     const page = await browser.newPage();
     await page.goto(`https://xepelin.com/blog/${category}`, {waitUntil: 'networkidle2'});
 
